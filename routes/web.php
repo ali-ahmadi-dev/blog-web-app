903929145f484dca15b3ab490446a6a6427fcdbc
ali ahmadi <aliahmadi1999001378@gmail.com>
2025-12-25 22:37:30 +0330

    Creating middleware

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,23 @@ use Illuminate\Support\Facades\Route;
 Route::view('/' , 'front.index')->name('home');
 
 
-Route::get('/dashboard', function () {
-    return view('backend.dashboard');
+
+Route::prefix('dashboard')->middleware('auth')->group(function(){
+    Route::middleware('role:admin')->group(function(){
+        Route::get('/', function () {
+            return view('backend.dashboard');
+        });
+
+    });
+
+//    Route::middleware('role: |')->group(function(){
+//
+//    });
+
+
 });
+
+
 
 
 
