23497e5f7c11f624ba0405f51660fdb49d8a6b75
ali ahmadi <aliahmadi1999001378@gmail.com>
2025-12-26 12:54:06 +0330

    Adding a relationship between tables

--- app/Models/dashboard/Article.php
+++ app/Models/dashboard/Article.php
@@ -3,8 +3,15 @@
 namespace App\Models\dashboard;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
     //
+
+    public function category(): BelongsTo
+    {
+
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Models/dashboard/Category.php
+++ app/Models/dashboard/Category.php
@@ -3,8 +3,13 @@
 namespace App\Models\dashboard;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+
+
+    public function articles(): HasMany{
+        return $this->hasMany(Article::class);
+    }
 }
